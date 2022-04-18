df59c245a628ee785c42fe218d8e015f30bf581f
B <sbslim.dev@gmail.com>
2022-04-18 14:59:08 +0100

    DAS-10

---
+++ src/App/Pages/Dashboard/Location/Location.tsx
@@ -0,0 +1,27 @@
+import "./location.scss";
+import Typography from "../../../ui-components/Typography/index";
+import { locationData } from "../../../Constants/data";
+import { Img } from "../../../ui-components";
+import img from "../../../../assets/images/img-california.jpg";
+import { Badge } from "@mui/material";
+
+function Location() {
+  console.log(img);
+  return (
+    <>
+      <div className="location_header boxHeader">
+        <Typography variant="h6" className="text-capitalize">
+          location
+        </Typography>
+        <Badge badgeContent={locationData.length} color="secondary" />
+      </div>
+      <div className="location_body flex">
+        {locationData.map((item, index) => {
+          return <Img key={index} {...item} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Location;
--- src/App/ui-components/Image/Img.tsx
+++ src/App/ui-components/Image/Img.tsx
@@ -1,13 +1,23 @@
+import Typography from "../Typography";
 import "./img.scss";
 
 interface IImg {
   size?: "small" | "medium" | "large";
-  src?: string;
-  alt?: string;
+  img?: string;
+  city?: string;
+  region?: string;
 }
 
-function Img({ size = "medium", src, alt }: IImg) {
-  return <img className={`img img--${size}`} src={src} alt={alt} />;
+function Img({ size = "medium", img, city, region }: IImg) {
+  return (
+    <div className={`img-root img--${size}`}>
+      <img className="img" src={img} alt={city} />
+      <div className="img_info text-capitalize">
+        <Typography variant="subtitle2">{city}</Typography>
+        <Typography variant="body2">{region}</Typography>
+      </div>
+    </div>
+  );
 }
 
 export default Img;
